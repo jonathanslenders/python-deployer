94bb7142a47b121808bef38e8b06f22c5a853017
Jonathan Slenders <jonathan.slenders@mobilevikings.com>
2012-12-17 14:43:24 +0100

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,13 @@ setup(
     long_description=open('README.md', 'r').read(),
     author='Jonathan Slenders, Mobile Vikings, City Live nv',
     packages=find_packages('.'),
+    install_requires = [
+        'paramiko==1.9.0',
+        'Twisted==12.2.0',
+        'pexpect==2.4',
+        'Pygments==1.5',
+        'termcolor==1.1.0',
+        ],
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
