eda6c8195a382239f7b67b669b07654561015b6d
Jonathan Slenders <jonathan@slenders.be>
2013-01-14 18:12:53 +0100

    initial_input for connect service.

--- deployer/contrib/services/connect.py
+++ deployer/contrib/services/connect.py
@@ -6,10 +6,18 @@ class Connect(Service):
     """
     Open SSH connection to host
     """
+    @property
+    def initial_input(self):
+        # Override this one in order to create a connect statement
+        # which does 'cd' to a certain directory, activates a virtualenv,
+        # or executes any other arbitrary command in the shell before handing
+        # over control to the user.
+        return None
+
     @default_action
     @isolate_one_only # It does not make much sense to open interactive shells to all hosts at the same time.
     def with_host(self):
-        self.host.start_interactive_shell()
+        self.host.start_interactive_shell(initial_input=self.initial_input)
         print
 
     @isolate_one_only
