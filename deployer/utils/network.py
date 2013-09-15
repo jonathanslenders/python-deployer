fd6064507ceb3a50301bfe67aca0819a9dad691d
Jonathan Slenders <jonathan@slenders.be>
2013-09-15 13:08:29 +0200

    More documentation in utils.

--- deployer/utils/network.py
+++ deployer/utils/network.py
@@ -1,28 +1,67 @@
 import re
 
+__all__ = ('parse_ifconfig_output', 'IfConfig', 'NetworkInterface')
+
+
 class NetworkInterface(object):
+    """
+    Information about a single network interface.
+    """
     def __init__(self, name='eth0'):
-        self.name = name
-        self.ip = None
+        self._name = name
+        self._ip = None
+
+    @property
+    def name(self):
+        """
+        Name of the network interface. e.g. "eth0".
+        """
+        return self._name
+
+    @property
+    def ip(self):
+        """
+        IP address of the network interface. e.g. "127.0.0.1"
+        """
+        return self._ip
 
     def __repr__(self):
         return 'NetworkInterface(name=%r, ip=%r)' % (self.name, self.ip)
 
 
 class IfConfig(object):
+    """
+    Container for the network settings, found by `ifconfig`.
+    This contains a list of :class:`NetworkInterface`.
+    """
     def __init__(self):
-        self.interfaces = []
+        self._interfaces = []
+
+    @property
+    def interfaces(self):
+        """
+        List of all :class:`NetworkInterface` objects.
+        """
+        return self._interfaces
 
     def __repr__(self):
         return 'IfConfig(interfaces=%r)' % self.interfaces
 
     def get_interface(self, name):
+        """
+        Return the :class:`NetworkInterface` object, given an interface name
+        (e.g. "eth0") or raise `AttributeError`.
+        """
         for i in self.interfaces:
             if i.name == name:
                 return i
         raise AttributeError
 
     def get_address(self, ip):
+        """
+        Return the :class:`NetworkInterface` object, given an IP addres
+        (e.g. "127.0.0.1") or raise `AttributeError`.
+        """
         for i in self.interfaces:
             if i.ip == ip:
                 return i
@@ -33,7 +72,13 @@ def parse_ifconfig_output(output, only_active_interfaces=True):
     """
     Parse the output of an `ifconfig` command.
 
-    :returns: A list of ``NetworkInterface`` objects.
+    :returns: A list of :class:`IfConfig` objects.
+
+    Example usage:
+    ::
+        ifconfig = parse_ifconfig_output(host.run('ifconfig'))
+        interface = ifconfig.get_interface('eth0')
+        print interface.ip
     """
     ifconfig = IfConfig()
     current_interface = None
@@ -50,10 +95,10 @@ def parse_ifconfig_output(output, only_active_interfaces=True):
             if current_interface:
                 # If this line contains 'inet'
                 for inet_addr in re.findall(r'inet (addr:)?(([0-9]*\.){3}[0-9]*)', l):
-                    current_interface.ip = inet_addr[1]
+                    current_interface._ip = inet_addr[1]
 
     # Return only the interfaces that have an IP address.
     if only_active_interfaces:
-        ifconfig.interfaces = filter(lambda i: i.ip, ifconfig.interfaces)
+        ifconfig._interfaces = filter(lambda i: i.ip, ifconfig._interfaces)
 
     return ifconfig
