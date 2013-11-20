dae8a5b204bcd5d4413c2549efb299587e717ea8
Jonathan Slenders <jonathan@slenders.be>
2013-11-20 06:32:20 +0100

    run_tests.py added.

---
+++ run_tests.py
@@ -0,0 +1,11 @@
+from tests.default_config_test import *
+from tests.host_container_test import *
+from tests.host_test import *
+from tests.inspector_test import *
+from tests.node_test import *
+from tests.query_test import *
+from tests.utils_test import *
+from tests.pty_test import *
+
+if __name__ == '__main__':
+    unittest.main()
